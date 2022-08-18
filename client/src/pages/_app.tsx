3742f997047f9affeea90ca3dd01e3968e461820
wolfhoons <wolfhoons@gmail.com>
2022-08-18 16:04:53 +0900

    Feat: auth context 추가 및 적용

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import axios from 'axios';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import axios from "axios";
+import { AuthProvider } from "../context/auth";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
 
-  axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + '/api';
-
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
