a851d8ed18f0b38e962e90494623ff6908e763fe
wolfhoons <wolfhoons@gmail.com>
2022-08-18 12:58:14 +0900

    Feat: axios baseURL 추가

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import axios from 'axios';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + '/api';
+
   return <Component {...pageProps} />
 }
 
