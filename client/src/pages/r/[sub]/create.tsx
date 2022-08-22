6b54082fdec52d7ee6c7f4cd63b31587a051e946
wolfhoons <wolfhoons@gmail.com>
2022-08-22 20:59:45 +0900

    Feat: 포스트 생성 페이지

---
+++ client/src/pages/r/[sub]/create.tsx
@@ -0,0 +1,89 @@
+import axios from "axios";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import React, { FormEvent, useState } from "react";
+import { Post } from "../../../types";
+
+const PostCreate = () => {
+  const router = useRouter();
+  const [title, set_title] = useState("");
+  const [body, set_body] = useState("");
+  const { sub: subName } = router.query;
+
+  const submitPost = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (title.trim() === '' || !subName) return;
+
+    try {
+      const { data: post } = await axios.post<Post>('/posts', {
+        title: title.trim(),
+        body,
+        sub: subName,
+      })
+
+      router.push(`/r/${subName}/${post.identifier}/${post.slug}`);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center pt-16">
+      <div className="w-10/12 mx-auto md:w-96">
+        <div className="p-4 bg-white rounded">
+          <h1 className="mb-3 text-lg">포스트 생성하기</h1>
+          <form onSubmit={submitPost}>
+            <div className="relative mb-2">
+              <input
+                type="text"
+                className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                placeholder="제목"
+                maxLength={20}
+                value={title}
+                onChange={(e) => set_title(e.target.value)}
+              />
+              <div
+                style={{ top: 10, right: 10 }}
+                className="absolute mb-2 text-sm text-gray-400 select-none"
+              >
+                {title.trim().length}/20
+              </div>
+            </div>
+            <textarea
+              rows={4}
+              placeholder="설명"
+              className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+              value={body}
+              onChange={(e) => set_body(e.target.value)}
+            />
+            <div className="flex justify-end">
+              <button className="px-4 py-1 text-sm font-semibold text-white bg-gray-400 border rounded">
+                생성하기
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostCreate;
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  try {
+    const cookie = req.headers.cookie;
+    if (!cookie) throw new Error("missing auth token cookie");
+
+    // cookie 가 있다면 쿠키로 서버에서 인증 처리
+    await axios.get("/auth/me", {
+      headers: { cookie },
+    });
+
+    return { props: {} };
+  } catch (e) {
+    res.writeHead(307, { Location: "/login" }).end();
+    return { props: {} };
+  }
+};
