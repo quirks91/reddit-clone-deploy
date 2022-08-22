f047f8de20631f8a9b6915e29a9c70df12ed0ac9
wolfhoons <wolfhoons@gmail.com>
2022-08-22 21:01:09 +0900

    Feat: 콘솔 삭제

--- server/src/middlewares/user.ts
+++ server/src/middlewares/user.ts
@@ -6,7 +6,6 @@ export default async (req: Request, res: Response, next: NextFunction) => {
   try {
     // 1. 요청 쿠키에 담겨 있는 토큰 정보 가져오기
     const token = req.cookies.token;
-    console.log("token:", token);
     if (!token) return next();
 
     // 2. verify method 와 jwt secret 을 이용해 토큰 Decoded
@@ -15,7 +14,6 @@ export default async (req: Request, res: Response, next: NextFunction) => {
     // 3. 토큰에서 나온 email 을 이용해 유저 정보 DB 에서 가져오기
     const user = await User.findOneBy({ email });
     if (!user) throw new Error("Unautenticated");
-    console.log("user:", user);
 
     // 3. 유저 정보를 res.local.user 에 넣어준다.
     res.locals.user = user;
