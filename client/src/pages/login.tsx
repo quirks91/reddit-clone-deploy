fa86ee251ca7ece21c7a36b124178f92e48613c2
wolfhoons <wolfhoons@gmail.com>
2022-08-22 20:59:12 +0900

    Refactor: 로그인페이지 href 수정

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -56,7 +56,7 @@ const login = () => {
           </form>
           <small>
             아직 아이디가 없나요?
-            <Link href="/login">
+            <Link href="/register">
               <a className="ml-1 text-blue-500 uppercase">회원가입</a>
             </Link>
           </small>
