922e82495a7429004abd167bb0494e4886bfae5c
wolfhoons <wolfhoons@gmail.com>
2022-08-23 13:12:08 +0900

    Feat: post Props 구조 분해 삭제

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -15,20 +15,7 @@ interface PostCardProps {
 }
 
 const PostCard = ({
-  post: {
-    identifier,
-    slug,
-    title,
-    body,
-    subName,
-    createdAt,
-    voteScore,
-    userVote,
-    commentCount,
-    url,
-    username,
-    sub,
-  },
+  post,
   mutate,
   subMutate,
 }: PostCardProps) => {
@@ -40,10 +27,10 @@ const PostCard = ({
   const vote = async (value: number) => {
     if (!authenticated) router.push("/login");
 
-    if (value === userVote) value = 0;
+    if (value === post.userVote) value = 0;
 
     try {
-      await axios.post("/votes", { identifier, slug, value });
+      await axios.post("/votes", { identifier: post.identifier, slug: post.slug, value });
       if (mutate) mutate();
       if (subMutate) subMutate();
     } catch (error) {
@@ -52,7 +39,7 @@ const PostCard = ({
   };
 
   return (
-    <div className="flex mb-4 bg-white rounded" id={identifier}>
+    <div className="flex mb-4 bg-white rounded" id={post.identifier}>
       {/* 좋아요 싫어요 기능 부분 */}
       <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
         {/* 좋아요 */}
@@ -60,19 +47,19 @@ const PostCard = ({
           className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
           onClick={() => vote(1)}
         >
-          {userVote === 1 ? (
+          {post.userVote === 1 ? (
             <FaArrowUp className="text-red-500" />
           ) : (
             <FaArrowUp />
           )}
         </div>
-        <p className="text-xs font-bold">{voteScore}</p>
+        <p className="text-xs font-bold">{post.voteScore}</p>
         {/* 싫어요 */}
         <div
           className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
           onClick={() => vote(-1)}
         >
-          {userVote === -1 ? (
+          {post.userVote === -1 ? (
             <FaArrowDown className="text-blue-500" />
           ) : (
             <FaArrowDown />
@@ -84,10 +71,10 @@ const PostCard = ({
         <div className="flex items-center">
           {!isInSubPage && (
             <div className="flex items-center">
-              <Link href={`/r/${subName}`}>
+              <Link href={`/r/${post.subName}`}>
                 <a>
                   <Image
-                    src={sub!.imageUrl}
+                    src={post.sub!.imageUrl}
                     alt="sub"
                     className="rounded-full cursor-pointer"
                     width={12}
@@ -95,9 +82,9 @@ const PostCard = ({
                   />
                 </a>
               </Link>
-              <Link href={`/r/${subName}`}>
+              <Link href={`/r/${post.subName}`}>
                 <a className="ml-2 text-xs font-bold cursor-pointer hover:underline">
-                  /r/{subName}
+                  /r/{post.subName}
                 </a>
               </Link>
               <span className="mx-1 text-xs text-gray-400">•</span>
@@ -106,26 +93,26 @@ const PostCard = ({
 
           <p className="text-xs text-gray-400">
             Posted by
-            <Link href={`/u/${username}`}>
-              <a className="mx-1 hover:underline">/u/{username}</a>
+            <Link href={`/u/${post.username}`}>
+              <a className="mx-1 hover:underline">/u/{post.username}</a>
             </Link>
-            <Link href={url}>
+            <Link href={post.url}>
               <a className="mx-1 hover:underline">
-                {dayjs(createdAt).format("YYYY-MM-DD HH:mm")}
+                {dayjs(post.createdAt).format("YYYY-MM-DD HH:mm")}
               </a>
             </Link>
           </p>
         </div>
 
-        <Link href={url}>
-          <a className="my-1 text-lg font-medium">{title}</a>
+        <Link href={post.url}>
+          <a className="my-1 text-lg font-medium">{post.title}</a>
         </Link>
-        {body && <p className="my-1 text-sm">{body}</p>}
+        {post.body && <p className="my-1 text-sm">{post.body}</p>}
         <div className="flex">
-          <Link href={url}>
+          <Link href={post.url}>
             <a>
               <i className="mr-1 fas fa-comment-alt fa-xs"></i>
-              <span>{commentCount}</span>
+              <span>{post.commentCount}</span>
             </a>
           </Link>
         </div>
