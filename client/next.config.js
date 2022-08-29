c33138c4f0dfdac8e6bd08e63c318c5dc4df607f
wolfhoons <wolfhoons@gmail.com>
2022-08-29 12:15:31 +0900

    Test: EC2 IP 주소 수정

--- client/next.config.js
+++ client/next.config.js
@@ -5,8 +5,7 @@ const nextConfig = {
     domains: [
       "www.gravatar.com",
       "localhost",
-      "ec2-15-165-111-70.ap-northeast-2.compute.amazonaws.com",
-      "15.165.111.70"
+      "ec2-52-79-236-103.ap-northeast-2.compute.amazonaws.com",
     ]
   }
 }
